78aef8d71014acd2b46bfe02756462a333e2140f
Justin Grant <jgrant27@gmail.com>
2014-01-15 16:37:03 -0800

    Update Rust palindrome code for Rust 0.9

--- rust/palindromes/src/palindromes.rs
+++ rust/palindromes/src/palindromes.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2009, Justin Grant <justin at imagine27 dot com>
+// Copyright (c) 2013, Justin Grant <justin at imagine27 dot com>
 // All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without modification,
@@ -26,7 +26,7 @@
 // EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#[link(name = "palindromes", vers = "1.0")];
+#[crate_id(name = "palindromes", vers = "1.0")];
 
 fn start_end(lengths: &~[int]) -> (uint, uint) {
   let mut i:uint = 0;
@@ -38,96 +38,21 @@ fn start_end(lengths: &~[int]) -> (uint, uint) {
       max = lengths[mi] as uint;
     }
     i += 1;
-  }  
-  let s:uint = uint::max(0, mi/2 - max/2);
+  }
+  let s:uint = std::num::max(0, mi/2 - max/2);
   (s, s + max)
 }
-
-// Finds the lengths of palindromes in a string.
-// O(n) time complexity. O(n) space complexity.
-pub fn pals_fast(text:&~str) -> (uint, uint) {
-  let tlen:int = text.len() as int;
-  let ilen:int = 2 * tlen + 1;
-  let mut lengths:~[int] = vec::build_sized(ilen as uint,
-                                            |f| {let mut i:int = 0;
-                                                 while i < ilen {
-                                                   f(0); i += 1; }});
-  let mut llen:int = 0; let mut j = 0; let mut d = 0; let mut s = 0;
-  let mut e = 0; let mut plen = 0; let mut k = 0;
-
-  if !lengths.is_empty() {
-    let mut i = 0; while i < tlen {
-      // is the string so far a palindrome ?
-      if i > plen && text[i - plen -1] == text[i] {
-        // yes, so we extend the current found palindrome length
-			  // and keep checking forward else ...
-        plen += 2;
-        i += 1;
-        loop;
-      }
-      // ... we have reached the end of the current found palindrome
-		  // so we set the current palindrome length etc. ...
-      lengths[k] = plen;
-      k += 1;
-      s = k - 2;
-      e = s - plen;
-      // ... then backtrack over the last found palindrome to see ...
-      let mut b = true;
-      j = s; while j > e {
-        j -= 1;
-        d = j - e;
-        // ... if we have a reflection of the same length ...
-        if lengths[j+1] == d {
-				  // ... yes, so we set new palindrome length ...
-          plen = d;
-				  // ... and stop backtracking.
-          b = false;
-          break;
-        }
-			  // ... no, so we take the smaller length,
-			  // update the lengths and continue backtracking.
-        lengths[k] = int::min(d, lengths[j+1]);
-        k += 1;
-      }
-      // if we we're backtracking then reset palindrome length
-		  // and move on.
-      if b {
-        plen = 1;
-        i += 1;
-      }
-    }	
-    // we're done scanning the text so we perform 
-	  // one last backtrack.
-    lengths[k] = plen;
-    k += 1;
-    llen = k;
-    s = llen - 2;
-    e = s - (2 * tlen + 1 - llen);
-    i = s; while i > e {
-      d = i - e - 1;
-      lengths[k] = int::min(d, lengths[i]);
-      k += 1;
-      i -= 1;
-    }
-  }
-  
-  start_end(&lengths)
-}
-
-    
-  
-
-
 
 // Finds the lengths of palindromes in a string.
 // O(n^2) time complexity. O(n) space complexity.
 pub fn pals_naive(text:&~str) -> (uint, uint) {
   let tlen:int = text.len() as int;
   let llen:int = if tlen > 1 { 2 * tlen + 1 } else { 0 };
-  let mut lengths:~[int] = vec::build_sized(llen as uint,
-                                            |f| {let mut i:int = 0;
-                                                 while i < llen {
-                                                   f(0); i += 1; }});
+  let size:Option<uint> = Some(llen as uint);
+  let mut lengths:~[int] = std::vec::build(size,
+                                           |f| {let mut i:int = 0;
+                                                while i < llen {
+                                                  f(0); i += 1; }});
   let mut i:int = 0;
   while i < llen {
     let mut start:int = i / 2;
@@ -140,6 +65,79 @@ pub fn pals_naive(text:&~str) -> (uint, uint) {
     lengths[i] = end - start;
     i += 1;
   }
+  start_end(&lengths)
+}
+
+// Finds the lengths of palindromes in a string.
+// O(n) time complexity. O(n) space complexity.
+pub fn pals_fast(text:&~str) -> (uint, uint) {
+  let tlen:int = text.len() as int;
+  let ilen:int = 2 * tlen + 1;
+  let size:Option<uint> = Some(ilen as uint);
+  let mut lengths:~[int] = std::vec::build(size,
+                                           |f| {let mut i:int = 0;
+                                                while i < ilen {
+                                                  f(0); i += 1; 
+                                                }});
+  let mut llen; let mut d; let mut s; let mut j; 
+  let mut e; let mut plen = 0; let mut k = 0;
+
+  if !lengths.is_empty() {
+    let mut i = 0; while i < tlen {
+      // is the string so far a palindrome ?
+      if i > plen && text[i - plen -1] == text[i] {
+        // yes, so we extend the current found palindrome length
+        // and keep checking forward else ...
+        plen += 2;
+        i += 1;
+        continue;
+      }
+      // ... we have reached the end of the current found palindrome
+      // so we set the current palindrome length etc. ...
+      lengths[k] = plen;
+      k += 1;
+      s = k - 2;
+      e = s - plen;
+      // ... then backtrack over the last found palindrome to see ...
+      let mut b = true;
+      j = s; while j > e {
+        j -= 1;
+        d = j - e;
+        // ... if we have a reflection of the same length ...
+        if lengths[j+1] == d {
+          // ... yes, so we set new palindrome length ...
+          plen = d;
+          // ... and stop backtracking.
+          b = false;
+          break;
+        }
+        // ... no, so we take the smaller length,
+        // update the lengths and continue backtracking.
+        lengths[k] = std::num::min(d, lengths[j+1]);
+        k += 1;
+      }
+      // if we are back-tracking then reset palindrome length
+      // and move on.
+      if b {
+        plen = 1;
+        i += 1;
+      }
+    }
+    // we're done scanning the text so we perform
+    // one last backtrack.
+    lengths[k] = plen;
+    k += 1;
+    llen = k;
+    s = llen - 2;
+    e = s - (2 * tlen + 1 - llen);
+    i = s; while i > e {
+      d = i - e - 1;
+      lengths[k] = std::num::min(d, lengths[i]);
+      k += 1;
+      i -= 1;
+    }
+  }
+
   start_end(&lengths)
 }
 
