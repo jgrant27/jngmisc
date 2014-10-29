d487884357d8c39cf798789db3b9c48aa493caaa
jgrant27 <jgrant27@gmail.com>
2014-10-28 20:39:56 -0700

    Add NuGet.exe ; Fix install-packages to run on OS X

--- fsharp/ring-probs/install-packages.sh
+++ fsharp/ring-probs/install-packages.sh
@@ -1,5 +1,6 @@
 #/usr/bin/env sh
 
+MONO_PATH="mono"
 NUGET_PATH="./packages/NuGet.exe"
 NUGET_OPTIONS="-OutputDirectory packages -ExcludeVersion -Verbosity normal"
 NUGET_PACKAGES="FAKE xunit"
@@ -7,7 +8,7 @@ NUGET_PACKAGES="FAKE xunit"
 for package in `echo $NUGET_PACKAGES`
 do
     echo "Installing package $package ..."
-    $NUGET_PATH install $package $NUGET_OPTIONS
+    $MONO_PATH $NUGET_PATH install $package $NUGET_OPTIONS
 done
 
 exit 0
