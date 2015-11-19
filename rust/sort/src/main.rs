e3a57149f8dff6cbe25a616182a442e9fd392f33
Justin Grant <jgrant27@gmail.com>
2015-11-18 21:32:29 -0800

    Parse args.

--- rust/sort/src/main.rs
+++ rust/sort/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    let cnt: u32 = match args.len() {
+        1 => 100,
+        _ => match args[1].parse::<u32>().ok() {
+            None => 100,
+            Some(cnt) => cnt,
+        },
+    };
+
+    println!("args {}", cnt);
 }
