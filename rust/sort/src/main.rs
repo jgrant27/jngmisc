9e2e9635a731b43d18f17ae548902debcc4d2aa2
Justin Grant <jgrant27@gmail.com>
2015-11-18 23:20:44 -0800

    Random vector generation.

--- rust/sort/src/main.rs
+++ rust/sort/src/main.rs
@@ -1,16 +1,42 @@
+extern crate rand;
+
 use std::env;
+
+use rand::Rng;
+
+
+fn quicksort(nums: Vec<u64>) -> Vec<u64> {
+    return nums;
+}
+
+fn gen_random_vec(cnt: u64) -> Vec<u64> {
+    let mut rng = rand::thread_rng();
+    let mut nums: Vec<u64> = Vec::new();
+    for _ in 0..cnt-1 {
+        nums.push(rng.gen_range(0, cnt));
+    }
+    return nums;
+}
 
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let cnt: u32 = match args.len() {
-        1 => 100,
-        _ => match args[1].parse::<u32>().ok() {
+    let cnt: u64 = match args.len() {
+        n if n <= 1 => 100,
+        _ => match args[1].parse::<u64>().ok() {
             None => 100,
             Some(cnt) => cnt,
         },
     };
 
-    println!("args {}", cnt);
+    let nums = gen_random_vec(cnt);
+    println!("Running sort funcs for {} random numbers :\n{:?}\n...",
+             cnt, nums);
+
+    let sorted_nums = quicksort(nums);
+
+    println!("Sorted:\n{:?}",
+             sorted_nums);
+
 }
