d24b21e0194c58dc44f9dde1258b76ed9952daee
Justin Grant <justin.grant@fieldsync.net>
2015-12-09 15:39:19 -0800

    avoid unnecessary String clone() in version of Rust permutations.

--- rust/permutations/src/jngmisc/permutations.rs
+++ rust/permutations/src/jngmisc/permutations.rs
@@ -2,12 +2,12 @@ extern crate rand;
 
 
 fn for_string_rec_aux(word: &String,
-                      new_word: &mut String,
+                      new_word: String,
                       used: &mut Vec<bool>,
                       level: usize,
                       perms: &mut Vec<String>) {
     if level == word.len() {
-        perms.push(new_word.clone());
+        perms.push(new_word);
     } else {
         for i in 0..used.len() {
             if *used.get_mut(i).unwrap() { continue; }
@@ -15,7 +15,7 @@ fn for_string_rec_aux(word: &String,
             let mut newer_word = new_word.clone();
             newer_word.push(word.chars().nth(i).unwrap());
             *used.get_mut(i).unwrap() = true;
-            for_string_rec_aux(word, &mut newer_word,
+            for_string_rec_aux(word, newer_word,
                                used, level+1, perms);
             *used.get_mut(i).unwrap() = false;
         }
@@ -25,9 +25,9 @@ fn for_string_rec_aux(word: &String,
 pub fn for_string_rec(word: String) -> Vec<String> {
     let mut perms = Vec::new();
     let mut used = Vec::new();
-    let mut new_word = String::new();
+    let new_word = String::new();
     for _ in 0..word.len() { used.push(false); }
-    for_string_rec_aux(&word, &mut new_word,
+    for_string_rec_aux(&word, new_word,
                        &mut used, 0, &mut perms);
     return perms;
 }
