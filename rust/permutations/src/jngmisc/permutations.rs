756ebbc7f6c01752b42d1a0a75048f22c8470027
Justin Grant <jgrant27@gmail.com>
2015-12-08 17:08:29 -0800

    Rust word permutations.

---
+++ rust/permutations/src/jngmisc/permutations.rs
@@ -0,0 +1,16 @@
+extern crate rand;
+
+
+fn for_string_rec_aux(word: String,
+                      used: Vec<Boolean>,
+                      mut perms: Vec<String>) -> Vec<String> {
+
+    perms
+}
+
+pub fn for_string_rec(word: String) -> Vec<String> {
+
+    let mut perms = Vec::new();
+    for_string_rec_aux(word, perms)
+
+}
---
+++ rust/permutations/src/main.rs
@@ -0,0 +1,35 @@
+extern crate rand;
+extern crate time;
+
+use std::env;
+use time::*;
+
+mod jngmisc;
+
+use jngmisc::*;
+
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    let default_word = String::from("abcd");
+    let word: String = match args.len() {
+        n if n <= 1 => default_word,
+        _ => match args[1].parse::<String>().ok() {
+            None => default_word,
+            Some(word) => word,
+        },
+    };
+
+    println!("\n\nGenerating all permutations for word '{}' ...",
+             word);
+
+    let start = precise_time_ns();
+    let perms = permutations::for_string_rec(word);
+    let end = precise_time_ns();
+    let duration_us = (end - start) / 1000;
+
+    println!("Permutations:\n{:?}\ntook {}us.",
+             perms, duration_us);
+
+}
