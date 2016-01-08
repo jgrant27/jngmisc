3e56635402d10eb698f12453f69f389dae58c2fd
Justin Grant <jgrant27@gmail.com>
2016-01-08 04:24:33 -0800

    First working version of rust radix sort.

--- rust/sort/src/jngmisc/sort/radixsort.rs
+++ rust/sort/src/jngmisc/sort/radixsort.rs
@@ -1,23 +1,22 @@
-pub fn radixsort(nums: Vec<u64>) -> Vec<u64> {
+pub fn radixsort(mut nums: Vec<u64>) -> Vec<u64> {
 
-    let mut zeros = &nums[1..50];
-    let mut ones = &nums[51..100];
+    let half_len = nums.len() / 2;
+    let mut ones = nums.split_off(half_len);
+    let mut zeros = nums;
 
-    for i in 0..63 {
-        let mut new_zeros = Vec::new();
-        let mut new_ones = Vec::new();
-        for j in 0..zeros.len()-1 {
-            let num = zeros[j];
-            let bit = num.rotate_right(i) & 1;
+    for i in 0..64 {
+        let mut new_zeros = Vec::with_capacity(half_len);
+        let mut new_ones = Vec::with_capacity(half_len);
+        for j in 0..zeros.len() {
+            let bit = zeros[j].rotate_right(i) & 1;
             if bit == 0 {
                 new_zeros.push(zeros[j]);
             } else {
                 new_ones.push(zeros[j]);
             }
         }
-        for k in 0..ones.len()-1 {
-            let num = ones[k];
-            let bit = num.rotate_right(k) & 1;
+        for k in 0..ones.len() {
+            let bit = ones[k].rotate_right(i) & 1;
             if bit == 0 {
                 new_zeros.push(ones[k]);
             } else {
