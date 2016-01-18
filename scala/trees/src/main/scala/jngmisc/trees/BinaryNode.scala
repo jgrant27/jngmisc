9f968156f689d525ad0b4f92f94b19b1fc0adebd
Justin Grant <jgrant27@gmail.com>
2016-01-18 10:19:45 -0800

    Minor code cleanup for trees (Scala)

--- scala/trees/src/main/scala/jngmisc/trees/BinaryNode.scala
+++ scala/trees/src/main/scala/jngmisc/trees/BinaryNode.scala
@@ -13,62 +13,49 @@ object BinaryNode {
 
   def createBalancedBinaryTree(items: Vector[Int]): BinaryNode[Int] = {
 
-    if (items != null && items.size < 1) {
-      null
-    } else {
-      val mid = items.size / 2
-      val left = createBalancedBinaryTree(items.slice(0, mid))
-      val right = createBalancedBinaryTree(items.slice(mid+1, items.size))
-      new BinaryNode(items(mid), left, right)
-    }
+    if (items != null && items.size < 1) return null
+
+    val mid = items.size / 2
+    val left = createBalancedBinaryTree(items.slice(0, mid))
+    val right = createBalancedBinaryTree(items.slice(mid+1, items.size))
+    new BinaryNode(items(mid), left, right)
 
   }
 
   def traverseBinaryTreeInOrder(
+
     node: BinaryNode[Int],
     fun: BinaryNode[Int] => Unit): Unit = {
 
-    if (null != node) {
-      traverseBinaryTreeInOrder(node.left, fun)
-      fun(node)
-      traverseBinaryTreeInOrder(node.right, fun)
-    }
+    if (null == node) return
+
+    traverseBinaryTreeInOrder(node.left, fun)
+    fun(node)
+    traverseBinaryTreeInOrder(node.right, fun)
 
   }
 
   def binaryTreeDepth(node: BinaryNode[Int]): Int = {
 
-    if (null != node) {
-      val leftDepth = binaryTreeDepth(node.left)
-      val rightDepth = binaryTreeDepth(node.right)
-      if (leftDepth > rightDepth) {
-        leftDepth + 1
-      } else {
-        rightDepth + 1
-      }
-    } else {
-      0
-    }
+    if (null == node) return 0
+
+    val leftDepth = binaryTreeDepth(node.left)
+    val rightDepth = binaryTreeDepth(node.right)
+    if (leftDepth > rightDepth) leftDepth + 1 else rightDepth + 1
 
   }
 
   def binaryTreeDepth2(node: BinaryNode[Int]): Int = {
 
-    if (null != node) {
-      val leftDepth = binaryTreeDepth2(node.left)
-      if (leftDepth == -1) return -1
-      val rightDepth = binaryTreeDepth2(node.right)
-      if (rightDepth == -1) return -1
-      val diff = (leftDepth - rightDepth).abs
-      if (diff > 1) return -1
-      if (leftDepth > rightDepth) {
-        leftDepth + 1
-      } else {
-        rightDepth + 1
-      }
-    } else {
-      0
-    }
+    if (null == node) return 0
+
+    val leftDepth = binaryTreeDepth2(node.left)
+    if (leftDepth == -1) return -1
+    val rightDepth = binaryTreeDepth2(node.right)
+    if (rightDepth == -1) return -1
+    val diff = (leftDepth - rightDepth).abs
+    if (diff > 1) return -1
+    if (leftDepth > rightDepth) leftDepth + 1 else rightDepth + 1
 
   }
 
