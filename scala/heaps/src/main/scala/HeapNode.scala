33172b914c7c0fe313f1b3e73a848b732a766b38
Justin Grant <Justin Grant jgrant27@gmail.com>
2016-01-19 15:12:13 -0800

    Initial project for Scala Heaps.

---
+++ scala/heaps/src/main/scala/HeapNode.scala
@@ -0,0 +1,20 @@
+package jngmisc.heaps
+
+
+import scala.collection.mutable
+
+
+
+class HeapNode[T]
+  (var data: T = null,
+    var left: HeapNode[T] = null,
+    var right: HeapNode[T] = null,
+    var parent: HeapNode[T] = null) {
+
+
+}
+
+
+object Heap {
+
+}
---
+++ scala/heaps/src/test/scala/HeapSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+
+import jngmisc.heaps._
+
+
+class HeapSpec extends FlatSpec with Matchers {
+
+  "A Heap" should "be correctly structured" in {
+
+  }
+
+}
