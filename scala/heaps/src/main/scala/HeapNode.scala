79215d113f05799da83c744a8e782af133eb95d6
Justin Grant <Justin Grant jgrant27@gmail.com>
2016-01-20 00:05:57 -0800

    first version of heap insert (Scala).

--- scala/heaps/src/main/scala/HeapNode.scala
+++ scala/heaps/src/main/scala/HeapNode.scala
@@ -2,19 +2,68 @@ package jngmisc.heaps
 
 
 import scala.collection.mutable
+import scala.math
+import scala.util.control.Breaks._
 
 
-
-class HeapNode[T]
-  (var data: T = null,
-    var left: HeapNode[T] = null,
-    var right: HeapNode[T] = null,
-    var parent: HeapNode[T] = null) {
-
+class BinaryHeapNode[T]
+  (var data: T = null) {
 
 }
 
 
-object Heap {
+class BinaryMinHeap[T]
+  () {
+
+  var nodes: Vector[BinaryHeapNode[Int]] = Vector()
+
+  def log2(x: Int) = math.log(x) / math.log(2)
+  def nodeLevel(pos: Int) = log2(pos).floor.toInt + 1
+  def parentPos(pos: Int): Int = {
+    val lastLevel = nodeLevel(pos)
+    val countWithoutCurrLevel =
+      math.pow(2, lastLevel - 1).toInt
+    val lastRowPos = pos - countWithoutCurrLevel
+    var parentPos = log2(lastRowPos).floor.toInt
+    parentPos = if (parentPos < 1) 0 else parentPos
+    parentPos = math.pow(2, lastLevel - 2).toInt + parentPos
+    parentPos
+  }
+
+  def insert(data: Int) = {
+    nodes = nodes :+ new BinaryHeapNode[Int](data)
+
+    if (nodes.size > 1) {
+      var parentInd = parentPos(nodes.size) - 1
+      var currInd = nodes.size - 1
+      var parent = nodes(parentInd)
+      var node = nodes(currInd)
+
+      while (nodes.size > 1 && currInd > 0 && parent.data > node.data) {
+        parent = nodes(parentInd)
+        node = nodes(currInd)
+
+        println(currInd + " => " + parentInd)
+        //println("replacing parent node " + parent.data + " with curr node " + node.data)
+        //println(parentInd + " " + currInd)
+        nodes = nodes.updated(parentInd, node)
+        nodes = nodes.updated(currInd, parent)
+        val prevParentInd = parentInd
+        parentInd = currInd
+        currInd = prevParentInd
+      }
+    }
+
+  }
+
+  def remove[T](data: T) = {
+
+  }
+
+}
+
+
+object BinaryMinHeap {
+
 
 }
--- scala/heaps/src/test/scala/HeapSpec.scala
+++ scala/heaps/src/test/scala/HeapSpec.scala
@@ -6,6 +6,13 @@ import jngmisc.heaps._
 class HeapSpec extends FlatSpec with Matchers {
 
   "A Heap" should "be correctly structured" in {
+    val heap = new BinaryMinHeap[Int]()
+    //Vector(3,7,2,9,1,4,10,8,6,5).foreach( num => heap.insert(num) )
+    (1 to 10).reverse.foreach( num => heap.insert(num) )
+      //(1 to 8).foreach( num => println(heap.nodeLevel(num)) )
+      //(1 to 8).foreach( num => println(num + " -> " + heap.parentPos(num)) )
+
+    heap.nodes.foreach(node => println(node.data))
 
   }
 
