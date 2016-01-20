92b6595dfd903eee94f9a3ae95356b639b837dca
Justin Grant <justin.grant@fieldsync.net>
2016-01-20 14:29:44 -0800

    Heap insert and remove functions (working) Scala.

--- scala/heaps/src/main/scala/HeapNode.scala
+++ scala/heaps/src/main/scala/HeapNode.scala
@@ -18,46 +18,90 @@ class BinaryMinHeap[T]
   var nodes: Vector[BinaryHeapNode[Int]] = Vector()
 
   def log2(x: Int) = math.log(x) / math.log(2)
-  def nodeLevel(pos: Int) = log2(pos).floor.toInt + 1
+  def nodeLevel(pos: Int) = log2(pos).floor.toInt
+
   def parentPos(pos: Int): Int = {
-    val lastLevel = nodeLevel(pos)
-    val countWithoutCurrLevel =
-      math.pow(2, lastLevel - 1).toInt
-    val lastRowPos = pos - countWithoutCurrLevel
-    var parentPos = log2(lastRowPos).floor.toInt
-    parentPos = if (parentPos < 1) 0 else parentPos
-    parentPos = math.pow(2, lastLevel - 2).toInt + parentPos
-    parentPos
+    val currLevel = nodeLevel(pos)
+    val countWithCurrLevel = math.pow(2, currLevel).toInt
+    val currRowPos = pos - countWithCurrLevel
+    val parentRowPos = log2(currRowPos).toInt
+    val parentLevelStartInd = math.pow(2, currLevel - 1).toInt
+    val parentPos = parentLevelStartInd + parentRowPos
+    if (parentPos < 0) 1 else parentPos
   }
 
-  def insert(data: Int) = {
-    nodes = nodes :+ new BinaryHeapNode[Int](data)
+  def firstChildPos(pos: Int): Int = {
+    val currLevel = nodeLevel(pos)
+    val countWithoutCurrLevel = math.pow(2, currLevel - 1).toInt
+    val currRowPos = pos - countWithoutCurrLevel
+    var childLevelStartPos = 2 * (currRowPos - 1)
+    childLevelStartPos = if (currLevel == 0) childLevelStartPos else childLevelStartPos + 1
+    val firstChildPos = childLevelStartPos + math.pow(2, currLevel).toInt
+    //println(s"\npos $pos currLevel ${currLevel} countWithoutCurrLevel ${countWithoutCurrLevel} currRowPos ${currRowPos} childLevelStartPos ${childLevelStartPos} firstChildPos ${firstChildPos}")
+    firstChildPos
+  }
 
-    if (nodes.size > 1) {
-      var parentInd = parentPos(nodes.size) - 1
-      var currInd = nodes.size - 1
-      var parent = nodes(parentInd)
-      var node = nodes(currInd)
+  def extractMin(): BinaryHeapNode[Int] = {
 
-      while (nodes.size > 1 && currInd > 0 && parent.data > node.data) {
-        parent = nodes(parentInd)
-        node = nodes(currInd)
+    if (nodes.size < 1) return null
 
-        println(currInd + " => " + parentInd)
-        //println("replacing parent node " + parent.data + " with curr node " + node.data)
-        //println(parentInd + " " + currInd)
-        nodes = nodes.updated(parentInd, node)
-        nodes = nodes.updated(currInd, parent)
-        val prevParentInd = parentInd
-        parentInd = currInd
-        currInd = prevParentInd
-      }
+    val min = nodes.head
+    nodes = nodes.updated(0, nodes(nodes.size-1)).dropRight(1)
+
+    if (nodes.size == 1) return min
+
+    var currInd = 0
+    var firstChildInd = firstChildPos(currInd+1) - 1
+    //println(s"(${pos-1}) ${curr.data} children are ($firstChildPos) ${leftChild.data} (${firstChildPos+1}) ${rightChild.data}")
+
+    while(true) {
+      if (!nodes.isDefinedAt(currInd)) return min
+      if (!nodes.isDefinedAt(firstChildInd)) return min
+
+      val curr: BinaryHeapNode[Int] = nodes(currInd)
+      val first: Int = nodes(firstChildInd).data
+      val second: Int = if (nodes.isDefinedAt(firstChildInd+1)) nodes(firstChildInd+1).data else first
+      val swapInd = if (first <= second) firstChildInd else firstChildInd + 1
+
+      if (curr.data <= nodes(swapInd).data) return min
+
+      //println(s"swapping nodes ${curr.data} <=> ${nodes(swapInd).data}")
+
+      nodes = nodes.updated(currInd, nodes(swapInd))
+      nodes = nodes.updated(swapInd, curr)
+      currInd = swapInd
+      firstChildInd = firstChildPos(currInd+1) - 1
     }
 
+    min
   }
 
-  def remove[T](data: T) = {
+  def insert(data: Int): Unit = {
+    nodes = nodes :+ new BinaryHeapNode[Int](data)
 
+    if (nodes.size <= 1) return
+
+    var parentInd = parentPos(nodes.size) - 1
+    var currInd = nodes.size - 1
+    var parent = nodes(parentInd)
+    var curr = nodes(currInd)
+
+    while (parent.data > curr.data) {
+      // swap
+      //println(s"Swapping parent ${parent.data} ($parentInd) with current ${curr.data} ($currInd)")
+      nodes = nodes.updated(parentInd, curr)
+      nodes = nodes.updated(currInd, parent)
+
+      // update
+      val prevCurrInd = currInd
+      currInd = parentInd
+      parentInd = parentPos(currInd + 1) - 1
+
+      if (parentInd < 0) return
+      parent = nodes(parentInd)
+      curr = nodes(currInd)
+      //println(s"New parent ${parent.data} ($parentInd) and current ${curr.data} ($currInd)")
+    }
   }
 
 }
--- scala/heaps/src/test/scala/HeapSpec.scala
+++ scala/heaps/src/test/scala/HeapSpec.scala
@@ -6,13 +6,21 @@ import jngmisc.heaps._
 class HeapSpec extends FlatSpec with Matchers {
 
   "A Heap" should "be correctly structured" in {
+    // Insert
     val heap = new BinaryMinHeap[Int]()
-    //Vector(3,7,2,9,1,4,10,8,6,5).foreach( num => heap.insert(num) )
-    (1 to 10).reverse.foreach( num => heap.insert(num) )
-      //(1 to 8).foreach( num => println(heap.nodeLevel(num)) )
-      //(1 to 8).foreach( num => println(num + " -> " + heap.parentPos(num)) )
+    Vector(3,7,2,9,1,4,10,8,6,5).foreach( num => heap.insert(num) )
+    //(1 to 10).reverse.foreach( num => heap.insert(num) )
+    heap.nodes.foreach(node => print(node.data + " "))
+    println("\n")
 
-    heap.nodes.foreach(node => println(node.data))
+    // Remove
+    heap.nodes.foreach( _ => {
+      var min = heap.extractMin()
+      val mindata = if (null == min) null else min.data
+      println("min: " + mindata)
+      heap.nodes.foreach(node => print(node.data + " "))
+      println()
+    })
 
   }
 
