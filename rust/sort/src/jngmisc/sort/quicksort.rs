df201a64e96e8be48decb404b3278941cc980ed8
Justin Grant <jgrant27@gmail.com>
2017-07-09 19:55:39 -0700

    Pick random pivot for Rust quicksort.

--- rust/sort/src/jngmisc/sort/quicksort.rs
+++ rust/sort/src/jngmisc/sort/quicksort.rs
@@ -1,3 +1,5 @@
+use rand::{thread_rng, Rng};
+
 pub fn quicksort_rec(nums: Vec<u64>) -> Vec<u64> {
 
     return match nums.len() {
@@ -5,11 +7,14 @@ pub fn quicksort_rec(nums: Vec<u64>) -> Vec<u64> {
         cnt => {
             let mut left = Vec::new();
             let mut right = Vec::new();
-            let pivot = nums[0];
-            for i in 1..cnt {
-                match nums[i] {
-                    num if num < pivot => left.push(num),
-                    num => right.push(num),
+            let pivot_index = thread_rng().gen_range(0, cnt);
+            let pivot = nums[pivot_index];
+            for i in 0..cnt {
+                if i != pivot_index {
+                    match nums[i] {
+                        num if num <= pivot => left.push(num),
+                        num => right.push(num),
+                    }
                 }
             }
             let mut left_sorted = quicksort_rec(left);
@@ -17,7 +22,7 @@ pub fn quicksort_rec(nums: Vec<u64>) -> Vec<u64> {
             left_sorted.push(pivot);
             left_sorted.append(&mut right_sorted);
             return left_sorted;
-        },
+        }
     };
 
 }
