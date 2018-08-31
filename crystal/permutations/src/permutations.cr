fe2fc89c16ddd51a2a81b480f4b70193fcb1d06a
Justin Grant <jgrant@imagine27.com>
2018-08-30 20:25:21 -0700

    refactor Crystal permutations

--- crystal/permutations/src/permutations.cr
+++ crystal/permutations/src/permutations.cr
@@ -1,26 +1,20 @@
 # TODO: Write documentation for `Permutations`
 
 def perms_for_str_aux (word, new_word, used)
-  if new_word.size == word.size
-    puts "#{new_word}"
-    return
-  end
+  (puts "#{new_word}" ; return) if new_word.size == word.size
 
-  (0..word.size-1).each do |i|
-    if used[i] == true
-      next
+  word.each_char_with_index do |ch, i|
+    if !used[i]
+      used[i] = true
+      perms_for_str_aux word, new_word + ch, used
+      used[i] = false
     end
-    used[i] = true
-    perms_for_str_aux word, new_word + word[i], used
-    used[i] = false
   end
 end
 
 
 def perms_for_str (word)
-  new_word = ""
-  used = Array(Bool).new(word.size) {|i| false}
-  perms_for_str_aux word, new_word, used
+  perms_for_str_aux word, "", Array.new word.size, false
 end
 
 perms_for_str "abc"
