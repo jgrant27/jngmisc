49ded6b29b7377ab6cbf11bdaa3a0bcb8ae92a83
Justin Grant <jgrant27@gmail.com>
2019-02-09 16:21:03 -0800

    Rust - Collatz conjecture

---
+++ rust/collatz/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+fn collatz(mut nums: Vec<u64>) -> Vec<u64> {
+    let n = nums[nums.len()-1];
+    if 1 == n {
+        nums
+    } else {
+        if 0 == n % 2 {
+            nums.push(n / 2);
+        } else {
+            nums.push(3 * n + 1);
+        }
+        collatz(nums)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let n = args[1].parse::<u64>().unwrap();
+    let res = collatz(vec![n]);
+    println!("{:?}", res);
+    println!("{} steps", res.len());
+}
