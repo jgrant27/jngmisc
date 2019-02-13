0994715a43f8723a8fb0659f2a9ee6cce23be32e
Justin Grant <jgrant27@gmail.com>
2019-02-12 17:02:13 -0800

    Rust - Collatz Conjecture (with bits)

--- rust/collatz/src/main.rs
+++ rust/collatz/src/main.rs
@@ -8,17 +8,35 @@ fn collatz(mut nums: Vec<u64>) -> Vec<u64> {
         if 0 == n % 2 {
             nums.push(n / 2);
         } else {
-            nums.push(3 * n + 1);
+            nums.push((3 * n) + 1);
         }
         collatz(nums)
     }
 }
 
+fn collatz_bits(mut nums: Vec<u64>) -> Vec<u64> {
+    let mut n = nums[nums.len()-1];
+    if 1 == n {
+        nums
+    } else {
+        if 0 == n % 2 {
+            n >>= 1;
+        } else {
+            n <<= 1;
+            n += nums[nums.len()-1] + 1;
+        }
+        nums.push(n);
+        collatz(nums)
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let n = args[1].parse::<u64>().unwrap();
     for i in 1..=n {
-        let res = collatz(vec![i]);
+        let res = collatz_bits(vec![i]);
+        //let res = collatz(vec![i]);
         println!("{:?}", res);
         println!("{} steps", res.len());
     }
