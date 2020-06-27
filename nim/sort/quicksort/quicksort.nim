2f968a2c114055484d880b10a12ec9742112ac8d
jay27 <jay@imagine27.com>
2020-06-26 19:48:37 -0700

    Nim : Refactor quicksort

--- nim/sort/quicksort/quicksort.nim
+++ nim/sort/quicksort/quicksort.nim
@@ -1,3 +1,6 @@
+import os, random, sequtils, strutils
+
+
 proc quickSort*[T](a: var openarray[T], inl = 0, inr = -1) =
   var r = if inr >= 0: inr else: a.high
   var l = inl
@@ -17,3 +20,16 @@ proc quickSort*[T](a: var openarray[T], inl = 0, inr = -1) =
       dec r
   quickSort(a, inl, r)
   quickSort(a, l, inr)
+
+
+let params = commandLineParams()
+let last = if params.len > 0:
+             parseUInt(params[0])
+           else:
+             100
+
+var a = (1'u..last).mapIt(rand(int(it))).toSeq()
+echo a
+
+quickSort a
+echo a
