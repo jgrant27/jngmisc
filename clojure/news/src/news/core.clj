75483f1fa1268269230d5e46341655ca02bb937b
Justin Grant <jgrant27@gmail.com>
2020-09-10 17:03:31 -0700

    Clojure - News : calc threads in pool ; print stories to fetch
    blah

--- clojure/news/src/news/core.clj
+++ clojure/news/src/news/core.clj
@@ -1,13 +1,12 @@
 (ns news.core
   (:gen-class)
   (:require [clojure.core.async :as async :refer [>! >!! <! <!!]]
-            [clj-http.client :as client]
-            [clj-http.conn-mgr :as conn-mgr]))
+            [clj-http [client :as client] [conn-mgr :as conn-mgr]]))
 
 (def ^:const HN_BASE_URL "https://hacker-news.firebaseio.com/v0")
 (def ^:const HN_TOP_STORIES_URL (format "%s/topstories.json" HN_BASE_URL))
 (def ^:const HN_STORY_BASE_URL (format "%s/item" HN_BASE_URL))
-(def ^:const THREADS_IN_POOL 64)
+(def ^:const THREADS_IN_POOL (* 10 (. (Runtime/getRuntime) availableProcessors)))
 
 (client/with-connection-pool {:timeout 3 :so-keep-alive true :threads THREADS_IN_POOL}
   (defn -get-story [i story_id]
@@ -23,10 +22,12 @@
         story_cnt (if (empty? args)
                     (count story_ids)
                     (min (Integer/parseInt (nth args 0)) (count story_ids)))
-        ch (async/merge
-            (for [[i story_id] (map-indexed vector (take story_cnt story_ids))]
-                  (async/go (-get-story (inc i) story_id)))
-            story_cnt)
-        stories (sort (for [_ (take story_cnt story_ids)] (<!! ch)))]
-    (doseq [[i story] stories] (println story))
-    (flush)))
+        story_ids (into [] (take story_cnt story_ids))]
+    (printf "%s stories to get ...\n%s\n\n" story_cnt story_ids)
+    (flush)
+    (let [ch (async/merge
+              (for [[i story_id] (map-indexed vector story_ids)]
+                (async/go (-get-story (inc i) story_id)))
+              story_cnt)
+          stories (sort (for [_ story_ids] (<!! ch)))]
+      (doseq [[i story] stories] (println story) (flush)))))
