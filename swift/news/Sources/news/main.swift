7b7fb378fc612f0e54dffac327cc205ecb0b8405
jgrant <jgrant@penguin>
2021-01-02 00:18:03 -0800

    Swift News - Fix warning ; don't output ids

--- swift/news/Sources/news/main.swift
+++ swift/news/Sources/news/main.swift
@@ -5,7 +5,7 @@ let STORIES_BASE_URL = "https://hacker-news.firebaseio.com/v0"
 let TOP_STORIES_URL = "\(STORIES_BASE_URL)/topstories.json"
 
 func jsonFromResponse(_ response: HTTPClient.Response) -> Any {
-    let bytes = try! response.body.flatMap { $0.getData(at: 0, length: $0.readableBytes) }
+    let bytes = response.body.flatMap { $0.getData(at: 0, length: $0.readableBytes) }
     let jsonString = String(bytes: bytes!, encoding: .utf8)!
     return try! JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!)
 }
@@ -13,7 +13,7 @@ func jsonFromResponse(_ response: HTTPClient.Response) -> Any {
 let client = HTTPClient(eventLoopGroupProvider: .createNew)
 
 let ids = jsonFromResponse(try! client.get(url: TOP_STORIES_URL).wait()) as! [UInt]
-print("Loading \(ids.count) news articles ... \n\(ids)\n")
+print("Loading \(ids.count) news articles ... \n")
 
 let allStoriesStr: [String] = ids.enumerated().map { (ind, id) in
     return (ind + 1, client.get(url: "\(STORIES_BASE_URL)/item/\(id).json"))
