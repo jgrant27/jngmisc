e8116ff2b4cb2a0a4acc96cc791cec9bbaf63508
Justin Grant <jgrant27@gmail.com>
2025-03-14 15:56:48 -0700

    Update Rust news to use lightweight tokio threads

--- rust/news/src/main.rs
+++ rust/news/src/main.rs
@@ -1,17 +1,28 @@
-use threadpool::ThreadPool;
-use std::sync::mpsc::channel;
+use futures::{stream, StreamExt};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use std::str;
+use tokio;
 
 const STORIES_URL: &str = "https://hacker-news.firebaseio.com/v0/topstories.json";
 const ITEM_URL_BASE: &str = "https://hacker-news.firebaseio.com/v0/item";
-const DEFAULT_THREAD_CNT: usize = 64;
 const DEFAULT_STORY_CNT: usize = 1000;
 
-fn no_title() -> String { "NO TITLE PROVIDED".to_string() }
-fn no_url() -> String { "NO URL PROVIDED".to_string() }
+fn no_n() -> u64 {
+    0
+}
+
+fn no_title() -> String {
+    "NO TITLE PROVIDED".to_string()
+}
+fn no_url() -> String {
+    "NO URL PROVIDED".to_string()
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Story {
+    #[serde(default = "no_n")]
+    n: u64,
     id: u64,
     #[serde(default = "no_title")]
     title: String,
@@ -19,38 +30,48 @@ struct Story {
     url: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = std::env::args().collect();
-    let mut story_cnt = match args.len() {
-        1 => DEFAULT_STORY_CNT,
-        _ => args[1].parse()?
-    };
-    let story_ids: Vec<u64> = reqwest::blocking::get(STORIES_URL)?.json::<Vec<u64>>()?;
-    story_cnt = std::cmp::min(story_cnt, story_ids.len());
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    let story_ids: Vec<u64> = client
+        .get(STORIES_URL)
+        .send()
+        .await
+        .unwrap()
+        .json::<Vec<u64>>()
+        .await
+        .unwrap();
+    let story_cnt = std::cmp::min(DEFAULT_STORY_CNT, story_ids.len());
     let story_ids = story_ids[0..story_cnt].to_vec();
-    let mut stories = vec![String::new(); story_cnt];
 
     println!("{} stories to get - {:?}\n", story_ids.len(), story_ids);
+    let mut n = 0;
+    let stories = stream::iter(story_ids)
+        .map(|id| {
+            let client = &client;
+            n += 1;
+            async move {
+                let surl = format!("{}/{}.json", ITEM_URL_BASE, &id);
+                let story_url = surl.as_str();
+                let resp = client.get(story_url).send().await.unwrap();
+                let b = resp.bytes().await;
+                let sl = b.unwrap().slice(..);
+                let mut story: Story = serde_json::from_slice(&sl).unwrap();
+                story.n = n;
+                story
+            }
+        })
+        .buffered(DEFAULT_STORY_CNT);
 
-    let pool = ThreadPool::new(DEFAULT_THREAD_CNT);
-    let (tx, rx) = channel();
+    stories
+        .for_each(|story| async move {
+            let story_str = format!(
+                "{} - {} - {} - {}",
+                story.n, story.id, story.title, story.url
+            );
+            println!("{}", story_str);
+        })
+        .await;
 
-    for (i, id) in story_ids.into_iter().enumerate() {
-        let tx = tx.clone();
-        pool.execute(move || {
-            let story_url = format!("{}/{}.json", ITEM_URL_BASE, &id);
-            let story: Story = reqwest::blocking::get(&story_url).unwrap().json::<Story>().unwrap();
-            let story_str = format!("{} - {} - {} - {}", i+1, id, story.title, story.url);
-            tx.send((i, story_str)).expect("channel should be waiting!");
-        });
-    }
-    for (i, story) in rx.iter().take(story_cnt) {
-        stories[i] = story;
-    }
-    for story in stories {
-        print!("{}\n", story);
-    }
     println!();
-
-    Ok(())
 }
